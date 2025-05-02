ad933af1bbdf44fecb94b4c81992a9da08aba728
Denis Molony <dmolony@users.noreply.github.com>
2025-05-03 09:58:11 +1000

    tidying

--- src/com/bytezone/diskbrowser2/gui/DBStatusBar.java
+++ src/com/bytezone/diskbrowser2/gui/DBStatusBar.java
@@ -46,13 +46,15 @@ public class DBStatusBar extends StatusBar implements FilterChangeListener
   private void setStatusText ()
   // ---------------------------------------------------------------------------------//
   {
-    String filterText = filterStatus.filterActive ? filterStatus.filterValue.isEmpty () ? "NONE" :
-    //          (filterStatus.filterReverse ? "~" : "") + 
-        filterStatus.filterValue : "OFF";
+    String filterText =
+        filterStatus.filterActive ? filterStatus.filterValue.isEmpty () ? "NONE" :
+        //          (filterStatus.filterReverse ? "~" : "") + 
+            filterStatus.filterValue : "OFF";
 
     String showText = (filterStatus.filterActive && !filterStatus.filterValue.isEmpty ())
         ? filterStatus.filterExclusion ? "Filtered lines" : "All lines" : "All lines";
 
-    statusDisplay.setText (String.format ("Filter: %-20s Show: %-20s", filterText, showText));
+    statusDisplay
+        .setText (String.format ("Filter: %-20s Show: %-20s", filterText, showText));
   }
 }
